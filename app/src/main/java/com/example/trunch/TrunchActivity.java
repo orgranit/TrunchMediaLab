2b34d622d1c480ec3fd2cc7bbaff31f0f21e5940
OrG <orgranit1989@gmail.com>
2015-04-05 15:32:47 +0300

    Added TrunchActivity

---
+++ app/src/main/java/com/example/trunch/TrunchActivity.java
@@ -0,0 +1,59 @@
+package com.example.trunch;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.Window;
+import android.widget.ImageView;
+
+/**
+ * Created by or on 4/3/2015.
+ */
+public class TrunchActivity extends Activity {
+
+
+    //=========================================
+    //				Constants
+    //=========================================
+    private static final long REPEAT_TIME = 1000 * 5;
+    private static final String SHARED_PREF_NAME = "com.package.SHARED_PREF_NAME";
+    private static final String SHARED_PREF_HAS_TRUNCH = "com.package.SHARED_PREF_HAS_TRUNCH";
+    private static final String SHARED_PREF_TRUNCHERS = "com.package.SHARED_PREF_TRUNCHERS";
+
+
+    //=========================================
+    //				Fields
+    //=========================================
+    ImageView mMatchScreen;
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.its_a_trunch);
+        mMatchScreen = (ImageView) findViewById(R.id.matchScreen);
+        String restName = getIntent().getStringExtra("restName");
+        String trunchers = getIntent().getStringExtra("trunchers");
+        showTrunchDialog(trunchers,restName);
+
+    }
+
+    public void showTrunchDialog(String trunchers,String restaurant) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Trunch!");
+        builder.setMessage("You are having lunch with: " + trunchers + "at "+ restaurant );
+        builder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+}
